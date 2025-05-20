42f3e9179ec48ef519539c47e173fdea7ad7088f
ngochtpt00025 <ngochtpt00025@gmail.com>
2025-05-20 22:11:22 +0700

    upload project

---
+++ teeshirt/src/main/java/com/datn/teeshirt/TeeshirtApplication.java
@@ -0,0 +1,13 @@
+package com.datn.teeshirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeeshirtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeeshirtApplication.class, args);
+	}
+
+}
